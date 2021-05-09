98297954afa88660e230c7866801655dbc907fad
Yuri R <yuri.flash@gmail.com>
2021-05-09 22:32:02 +1000

    Adding conversion test from JPEG to RGB 888.

--- Embedded JPEG converter/main.cpp
+++ Embedded JPEG converter/main.cpp
@@ -5,8 +5,19 @@
 
 using namespace std;
 
+
+long GetFileSize(std::string filename)
+{
+    struct stat stat_buf;
+    int rc = stat(filename.c_str(), &stat_buf);
+    return rc == 0 ? stat_buf.st_size : -1;
+}
+
+
 int main()
 {
+    // Converting file RGB 888 to JPEG ///////////////////
+
     cout<<"Hello World";
     camera_fb_t imageTest;
     imageTest.format = PIXFORMAT_RGB888;
@@ -26,5 +37,28 @@ int main()
 
     frame2jpg_cb(&imageTest, 100, jpg_encode_file, nullptr);
     fclose(file);
+
+    ///////////////// Convertion Done ///////////////////
+
+    // Converting from JPEG to RGM 888///////////////////
+
+    uint32_t fileSize = GetFileSize("output.jpeg");
+    uint8_t* jpegFile = (uint8_t*)malloc(fileSize);
+    memset(jpegFile, 0, fileSize);
+
+    if ((file = fopen("output.jpeg","rb")) == NULL)
+    {
+        printf("Error! opening file");
+        return 0;
+    }
+
+    size_t result = fread(jpegFile, 1, fileSize, file);
+
+    static uint8_t result888[640*480*3];
+
+    fmt2rgb888(jpegFile, fileSize, pixformat_t::PIXFORMAT_JPEG, result888);
+    
+    ///////////////// Convertion Done ///////////////////
+
     return 0;
 }
